27d4e1dd81533392ba4db5d60811f997fef98546
Manoela Urias <maanoelaurias@gmail.com>
2024-10-29 10:22:30 -0300

    criacao

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import HomeView from './views/HomeView.vue';
+
+</script>
+
+<template>
+    <div>
+      <HomeView/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import UploadFile from "../components/UploadFile/UploadFile.vue";
+</script>
+
+<template>
+  <main>
+    <UploadFile/>
+  </main>
+</template>
